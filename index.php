f95f51ca24bbecea06ef09ba460ba40dbbb653b1
marc <marc@marcfischer.org>
2018-03-19 21:24:32 +0100

    modified analyzer for naser, edited crawler for second menu

--- index.php
+++ index.php
@@ -9,18 +9,18 @@ include 'vendor/autoload.php';
 $parser = new \Smalot\PdfParser\Parser();
 
 // keywords um pdfs zu durchsuchen
-$keywords = array("Montag","Dienstag","Mittwoch","Donnerstag","Freitag");
-$regex_keywords = array("M\s*o\s*n\s*t\s*a\s*g","D\s*i\s*e\s*n\s*s\s*t\s*a\s*g","M\s*i\s*t\s*t\s*w\s*o\s*c\s*h","D\s*o\s*n\s*n\s*e\s*r\s*s\s*t\s*a\s*g","F\s*r\s*e\s*i\s*t\s*a\s*g");
+$keywords = array("Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag");
+$regex_keywords = array("M\s*o\s*n\s*t\s*a\s*g", "D\s*i\s*e\s*n\s*s\s*t\s*a\s*g", "M\s*i\s*t\s*t\s*w\s*o\s*c\s*h", "D\s*o\s*n\s*n\s*e\s*r\s*s\s*t\s*a\s*g", "F\s*r\s*e\s*i\s*t\s*a\s*g");
 
 // herstellernamen
 $names_to_pdfids = array(
-    1 => "Naser",
-    2 => "Dees"
+  1 => "Naser",
+  2 => "Dees"
 );
 
 // new dees pdf crawler
 $data = file_get_contents("http://www.metzgerei-dees.de/"); // get source from site
-$hyperfile = htmlspecialchars($data);   
+$hyperfile = htmlspecialchars($data);
 preg_match("/metzgerei-dees.de\/wp-content\/themes\/metzgereidees\/uploads\/m(.+)pdf/isU", $hyperfile, $deeslink);  // regex to search for menu[].pdf
 //echo $deeslink[0];  //
 $deesstring = "http://www."; // build link string
@@ -28,8 +28,8 @@ $deesstring .= $deeslink[0];
 
 
 $links_to_pdfs = array(
-    1 => "http://www.wurstnaser.de/Speiseplan1.pdf",
-    2 =>  $deesstring           //"http://www.metzgerei-dees.de/wp-content/themes/MetzgereiDees/uploads/Menüplan-für-die-Woche-KW-7-2018-12.02-17.02.2018-neu.pdf"
+  1 => "http://www.wurstnaser.de/Speiseplan1.pdf",
+  2 => $deesstring           //"http://www.metzgerei-dees.de/wp-content/themes/MetzgereiDees/uploads/Menüplan-für-die-Woche-KW-7-2018-12.02-17.02.2018-neu.pdf"
 );
 
 
@@ -39,11 +39,11 @@ $allMenues = array();
 
 // pdf dateeien parsen
 for ($currentPdf = 1; $currentPdf <= count($links_to_pdfs); $currentPdf++) {
-    $pdf    = $parser->parseFile($links_to_pdfs[$currentPdf]);
+  $pdf = $parser->parseFile($links_to_pdfs[$currentPdf]);
 // details
-    $allAuthors[$currentPdf]  = $pdf->getDetails()["Author"];
+  $allAuthors[$currentPdf] = $pdf->getDetails()["Author"];
 // text
-    $allMenues[$currentPdf] = $pdf->getText();
+  $allMenues[$currentPdf] = $pdf->getText();
 }
 
 //var_dump($allMenues[2]);
@@ -58,17 +58,17 @@ $parsedMenu = array();
 
 // regex für dees: 
 array_push($regex_keywords, "g\s*e\s*n\s*i");
-for($ii = 0; $ii <= count($regex_keywords) - 2; $ii++) {
+for ($ii = 0; $ii <= count($regex_keywords) - 2; $ii++) {
     //echo "<b>$keywords[$ii]:</b><br>";
-    preg_match("/".$regex_keywords[0]."(.+)g\s*e\s*n\s*i/isU", $allMenues[2], $output_array);
-    preg_match("/".$regex_keywords[$ii]."(.+)".$regex_keywords[$ii+1]."/isU", $output_array[0], $output_array1);
-    preg_match("/".$regex_keywords[$ii]."(.+)€/isU", $output_array1[0], $output_array2);
-    preg_match("/€(.+)€/isU", $output_array1[0], $output_array3);
+  preg_match("/" . $regex_keywords[0] . "(.+)g\s*e\s*n\s*i/isU", $allMenues[2], $output_array);
+  preg_match("/" . $regex_keywords[$ii] . "(.+)" . $regex_keywords[$ii + 1] . "/isU", $output_array[0], $output_array1);
+  preg_match("/" . $regex_keywords[$ii] . "(.+)€/isU", $output_array1[0], $output_array2);
+  preg_match("/€(.+)€/isU", $output_array1[0], $output_array3);
     //echo($output_array2[1]);
-    $parsedMenu[$keywords[$ii]]["2"]["1"] = $output_array2[1];
+  $parsedMenu[$keywords[$ii]]["2"]["1"] = $output_array2[1];
     //echo "<br>";
     //echo($output_array3[1]);
-    $parsedMenu[$keywords[$ii]]["2"]["2"] = $output_array3[1];
+  $parsedMenu[$keywords[$ii]]["2"]["2"] = $output_array3[1];
     //echo "<br>";
 }
 
@@ -77,44 +77,38 @@ for($ii = 0; $ii <= count($regex_keywords) - 2; $ii++) {
 // regex für Naser:
 //var_dump($allMenues[1]);
 //echo "<br>";
+
+// new v1.2 naser pdf analyzer
+// get monday of current week
+
+$wochentag = strftime("%w", mktime(0, 0, 0, date("m"), date("d"), date("Y"))) - 1;
+if ($wochentag == -1) $wochentag = 6;
+$monday = date("d.m", mktime(0, 0, 0, date("m"), date("d") - $wochentag, date("Y")));
+
+
 array_pop($regex_keywords);
 array_push($regex_keywords, "essen");
 
-for($ii = 0; $ii <= count($regex_keywords) - 2; $ii++) {
+for ($ii = 0; $ii <= count($regex_keywords) - 2; $ii++) {
     //echo "<b>$keywords[$ii]:</b><br>";
-preg_match("/Tagesessen(.+)rungen/isU", $allMenues[1], $output_array);
-preg_match("/".$regex_keywords[$ii]."(.+)".$regex_keywords[$ii+1]."/isU", $output_array[0], $output_array1);
+  preg_match("/Tagesessen(.+)rungen/isU", $allMenues[1], $output_array);
+  preg_match("/" . $regex_keywords[$ii] . "(.+)" . $regex_keywords[$ii + 1] . "/isU", $output_array[0], $output_array1);
 //var_dump($output_array1);
 
+
+
+
+
 // Tagesgericht 1
-preg_match("/1(.+)2/isU", $output_array1[0], $output_array2);
+  preg_match("/1(.+)2/isU", $output_array1[0], $output_array2);
 
 // Tagesgericht 2
-preg_match("/2(.+)\b/ism", $output_array1[0], $output_array3);
-preg_match("/2(.+)\b/ism", $output_array3[1], $output_array4);
-preg_match("/2(.+)".$regex_keywords[$ii+1]."/ism", $output_array4[1], $output_array5);
-preg_match("/2(.+)".$regex_keywords[$ii+1]."/ism", $output_array5[1], $output_array6);
+  preg_match("/1\s*8\s*2(.+)\b/ism", $output_array1[1], $output_array3);        // Durchsuche nach "1 8 2", egal was dazwischen steht (Jahreszahl '18, Gericht 2), funktioniert ggf. nicht am 18.02. oder an Tagen, an denen die kombo vorkommt
+  preg_match("/2(.+)" . $regex_keywords[$ii + 1] . "/ism", $output_array3[2], $output_array4);      // Trimmt den Wochentag weg
 
+  $parsedMenu[$keywords[$ii]]["1"]["1"] = $output_array2[1];
+  $parsedMenu[$keywords[$ii]]["1"]["2"] = $output_array3[1];
 
-//echo($output_array2[1]);
-$parsedMenu[$keywords[$ii]]["1"]["1"] = $output_array2[1];
-
-//echo "<br>";
-if(!empty($output_array6)) {
-    ////echo($output_array6[1]);
-    $parsedMenu[$keywords[$ii]]["1"]["2"] = $output_array6[1];
-}
-elseif(!empty($output_array5)){
-    preg_match("/2(.+)".$regex_keywords[$ii+1]."/ism", $output_array4[1], $output_array5);
-    ////echo($output_array5[1]);
-    $parsedMenu[$keywords[$ii]]["1"]["2"] = $output_array5[1];
-}
-else {
-    preg_match("/2(.+)".$regex_keywords[$ii+1]."/ism", $output_array3[1], $output_array4);
-    ////echo($output_array4[1]);
-    $parsedMenu[$keywords[$ii]]["1"]["2"] = $output_array4[1];
-}
-//echo "<br>";
 }
 array_pop($regex_keywords);       // letztes word wieder aus den keywords löschen
 //var_dump($parsedMenu);
@@ -160,12 +154,13 @@ array_pop($regex_keywords);       // letztes word wieder aus den keywords lösch
       <nav class="nav nav-underline">
         <a class="nav-link current" href="#">Analysierte pdfs:</a>
         <?php 
-        foreach($links_to_pdfs as $id => $value ){
-            ?>
+        foreach ($links_to_pdfs as $id => $value) {
+          ?>
             <a class="nav-link" href="<?php echo $value; ?>"><?php echo $names_to_pdfids[$id]; // analysierte pdf-dateien ausgeben (quellen)?></a>       
         <?php
-        }; 
-        ?>
+
+      };
+      ?>
       </nav>
     </div>
 
@@ -179,8 +174,8 @@ array_pop($regex_keywords);       // letztes word wieder aus den keywords lösch
       </div>
 
 
-<?php   for($i = 0; $i <= count($keywords) - 1; $i++) {     // Menüliste ausgeben
-    ?>
+<?php for ($i = 0; $i <= count($keywords) - 1; $i++) {     // Menüliste ausgeben
+  ?>
 
 
       <div class="my-3 p-3 bg-white rounded box-shadow">
@@ -200,7 +195,8 @@ array_pop($regex_keywords);       // letztes word wieder aus den keywords lösch
           </p>
         </div>
       </div>
-    <?php     }; ?>
+    <?php 
+  }; ?>
 
 
 
@@ -209,12 +205,13 @@ array_pop($regex_keywords);       // letztes word wieder aus den keywords lösch
         <div class="lh-100">
           <h6 class="mb-0 lh-100">Analysierte PDFs:</h6>
           <?php 
-        foreach($links_to_pdfs as $id => $value ){
+          foreach ($links_to_pdfs as $id => $value) {
             ?>
             <small><?php echo $names_to_pdfids[$id]; ?>, erstellt von <?php echo $allAuthors[$id]; ?> <a class="text-info" href="<?php echo $value; ?>">Link</a></small><br>
         <?php
-        }; // analysierte pdf-dateien und deren autoren
-        ?>
+
+      }; // analysierte pdf-dateien und deren autoren
+      ?>
         </div>
       </div>
       
@@ -224,7 +221,7 @@ array_pop($regex_keywords);       // letztes word wieder aus den keywords lösch
           <br>
           <p>Ich stehe in keiner Verbindung zu den o.g. Restaurants/Metzgereien/Bistros etc. <br> Ich beziehe durch diese Seite keine Vorzüge. <br> Diese Seite dient ledeglich der Information.<br>Alle Angaben ohne Gewähr</p>
           <br>
-          <a href="https://github.com/kartoffelfighter/hunger">get the source code</a><br>
+          <a class="text-info" href="https://github.com/kartoffelfighter/hunger">get the source code</a><br>
           <small class="text-light">Impressum: Verantwortlich für den Inhalt dieser Seite (sofern nicht automatisch durch algorithmen ausgewerterte Informationen): Marc Fischer (Kontakt: marc{a}marcfischer.org)</small>
         </div>
       </div>
